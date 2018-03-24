9a8a8e4da6fa3c9f1daa4417e5405c73d9a5ebc7
hudginspj <hudginspj@mymail.vcu.edu>
2018-03-23 21:50:21 -0400

    examples

---
+++ experiments/xTrain.py
@@ -0,0 +1,34 @@
+import collector
+
+NUM_TRAINING_POINTS = 100
+training_points = [] #structures
+training_windows = [] #numpy arrays
+training_outputs = []
+
+simData = None #TODO
+collector = Collector()
+for i in range(NUM_TRAINING_POINTS):
+    point = simData.nextPoint()
+    collector.addPoint(point)
+    window = collector.nextXWindow()
+    if window:
+        training_windows.append(window)
+        training_outputs.append(collector.nextYOutputs)
+    
+    trainer = None #TODO
+    trainer.train(training_windows, training_outputs)
+
+
+
+# Next do the predictions
+
+
+
+
+
+
+
+
+
+
+
