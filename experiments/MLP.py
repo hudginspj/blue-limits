971bbbc42b5ab88d9faecbf29a947170474475ad
beye1 <beyep@mymail.vcu.edu>
2018-03-23 21:57:45 -0400

    stash

--- experiments/MLP.py
+++ experiments/MLP.py
@@ -2,9 +2,8 @@ import numpy as np, math, pandas as pds
 from sklearn import cross_validation, preprocessing,svm
 
 
-def regressor(**teledata):
-    dataframe = pds.DataFrame(columns=('Mode','OrbitAngle','Temperature','Battery'))
-    dataframe = dataframe.append({'Mode':teledata['Mode'],'OrbitAngle','Temperature','Battery'})
+dataframe = pds.DataFrame(columns=('Mode','OrbitAngle','Temperature','Battery'))
+dataframe = np.array(dataframe.append(teledata))
 
 
 
