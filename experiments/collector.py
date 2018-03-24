2dce757243e4d139c88c99f0e2e8b6cfbe2b0e97
hudginspj <hudginspj@mymail.vcu.edu>
2018-03-23 20:12:51 -0400

    example and collector

---
+++ experiments/collector.py
@@ -0,0 +1,48 @@
+
+MIN_WINDOWS = 10
+WINDOW_SIZE = 10
+
+
+class Collector(object):
+
+
+    # The class "constructor" - It's actually an initializer 
+    def __init__(self, columns):
+        self.columns = columns
+        self.points = []
+        self.counter = -1
+
+    def addPoint(self, point):
+        self.points.append(point)
+        self.counter += 1
+
+    def nextXWindow(self):
+        if self.counter >= WINDOW_SIZE + 1:
+            ret = []
+            for i in range(WINDOW_SIZE):
+                point = self.points[self.counter - i - 1]
+                ret.append(point[0])
+            return ret
+        else:
+            return None
+
+    def nextYValue(self):
+        if self.counter >= WINDOW_SIZE + 1:
+            return self.points[self.counter][0]
+        else:
+            return None
+
+
+
+if __name__ == "__main__":
+    c = Collector(5)
+    for i in range(100):
+        c.addPoint([i])
+    print(c.nextXWindow())
+    print(c.nextYValue())
+
+
+
+
+
+
