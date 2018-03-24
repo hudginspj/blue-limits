3751267fc7d4b6abf987e219c6fda3d5c037e083
hudginspj <hudginspj@mymail.vcu.edu>
2018-03-24 00:51:58 -0400

    regressor should be working

--- experiments/xRegressor.py
+++ experiments/xRegressor.py
@@ -13,18 +13,20 @@ class Regressor(object):
         for i in range(num_outputs):
             self.regressors.append(self.makeRegressor())
 
-    def predict(self, xWindows):
-        assert xWindows[0].__len__() >= 1
-        return [1.0, -5.0]
+    def predict(self, xWindow):
+        yOutputs = []
+        for i_out in range(self.num_outputs):
+            prediction = self.regressors[i_out].predict(xWindow)
+        return yOutputs
 
     def train(self, xWindows, yOutputs):
         xWindowArr = numpy.array(xWindows)
-        for i_out in range(num_outputs):
+        for i_out in range(self.num_outputs):
             ys = []
             for i in range(yOutputs.__len__()):
-                ys.append(yOutputs[i][out])
-            yWindowArr = numpy.array(ys)
-            regressors[i_out].fit(xWindows, ys)]
+                ys.append(yOutputs[i][i_out])
+            yArr = numpy.array(ys)
+            self.regressors[i_out].fit(xWindowArr, yArr)
         # assert yOutputs[0].__len__() >= 1
         # assert xWindows[0].__len__() >= 1
 
@@ -32,25 +34,7 @@ class Regressor(object):
         r = RandomForestRegressor()
         return r
 
-    def addPoint(self, point):
-        self.points.append(point)
-        self.counter += 1
 
-#     def nextXWindow(self):
-#         if self.counter >= WINDOW_SIZE + 1:
-#             ret = []
-#             for i in range(WINDOW_SIZE):
-#                 point = self.points[self.counter - i - 1]
-#                 ret.append(point[0])
-#             return ret
-#         else:
-#             return None
-
-#     def nextYOutputs(self):
-#         if self.counter >= WINDOW_SIZE + 1:
-#             return [self.points[self.counter][0]]
-#         else:
-#             return None
 
 
 # # clf = sklearn.svm.SVR(gamma=0.001, C=100.)
