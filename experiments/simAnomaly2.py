c58aa2550f996382b3c6b035946f057b7fd9b78c
hudginspj <hudginspj@mymail.vcu.edu>
2018-03-25 04:31:57 -0400

    periodic data

--- experiments/simAnomaly2.py
+++ experiments/simAnomaly2.py
@@ -5,13 +5,20 @@ a_counter = 0
 def addAnomaly(point):
     global a_counter
     a_counter += 1
+    mod = a_counter % 180
+    dif = 0
+    if (mod > 70 and mod <=80):
+        dif = (mod - 70) * -0.03
+    point[1]['TEMP2'] += dif 
+
+def addAnomaly2(point):
+    global a_counter
+    a_counter += 1
     mod = a_counter % 120
     dif = 0
     if (mod > 60 and mod <=80):
         dif = (mod - 60) * 0.2
     point[1]['ACCELX'] += dif 
-
-
 
 
 
