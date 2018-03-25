07a61d202e7d7c4ce604bb40aac0b244ae9d9003
beye1 <beyep@mymail.vcu.edu>
2018-03-25 05:14:33 -0400

    optimizing popup plots

--- experiments/popupplots.py
+++ experiments/popupplots.py
@@ -9,16 +9,13 @@ try:
     plt.ion()
     fig = plt.figure()
     clr = fig.add_subplot(1,1,1)
-    fig1 = fig.add_subplot(2, 1, 1)
-    # fig2 = fig.add_subplot(2, 3, 2)
-    # fig3 = fig.add_subplot(2, 3, 3)
-    fig4 = fig.add_subplot(2, 1, 2)
     font_dict = {'family': 'serif',
                  'color': 'green',
                  'size': 15}
 
     def graph(timestamps, realValues, predictedValues, b1, b2, anomaly=False, lm1=2.1, lm2=-2.1, redll=-2.4, redlh=2.4):
-        cnter = 1
+        clr.clear()
+        # cnter = 1
         lm1s = []
         lm2s = []
         lreds = []
@@ -31,47 +28,33 @@ try:
             lreds.append(redll)
         for i in range(timestamps.__len__()):
             hreds.append(redlh)
-        fig1.plot(timestamps, lm1s, 'yellow')
-        fig1.plot(timestamps, lm2s, 'yellow')
-        fig1.plot(timestamps, lreds, 'red')
-        fig1.plot(timestamps, hreds, 'red')
-        fig1.plot(timestamps, predictedValues, 'xkcd:green', label="Preds")
-        fig1.plot(timestamps, realValues, 'orange', label="Reals")
-        fig1.plot(timestamps, b1, 'blue', label="Bound1")
-        fig1.plot(timestamps, b2, 'blue', label="Bound2")
+        plt.plot(timestamps, lm1s, 'yellow')
+        plt.plot(timestamps, lm2s, 'yellow')
+        plt.plot(timestamps, lreds, 'red')
+        plt.plot(timestamps, hreds, 'red')
+        plt.plot(timestamps, predictedValues, 'xkcd:green', label="Preds")
+        plt.plot(timestamps, realValues, 'orange', label="Reals")
+        plt.plot(timestamps, b1, 'blue', label="Bound1")
+        plt.plot(timestamps, b2, 'blue', label="Bound2")
+        plt.title('Temperature', font_dict)
+        plt.draw()
+        plt.pause(0.01)
 
-
-        fig1.set_title('Temperature', font_dict)
-        # plt.legend()
         # fig1.get_legend()
         if anomaly:
-            plt.set_title('Anomaly', font_dict)
+            plt.title('Anomaly', font_dict)
             plt.annotate('',(timestamps[len(timestamps)-1],realValues[len(realValues)-1]),
                  xytext=(0.8, 0.9), textcoords='axes fraction',
                  arrowprops = dict(color='darkred'))
-            # plt.draw()
+            plt.draw()
+            plt.pause(0.01)
         else:
             pass
         plt.pause(0.01)
         # pic = fig1.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
         # fig.savefig('ax2_figure.png', bbox_inches=pic)
-        cnter +=1
-    def orbitangleplot(timestamp, orbitangle):
-        fig2.plot(timestamp, orbitangle)
-        fig2.set_title('Orbit Angle', font_dict)
-        plt.pause(0.01)
-    def accelxplot(timestamps, accelx):
-        fig3.plot(timestamps, accelx, 'red')
-        fig3.set_title('Acceleration', font_dict)
-        plt.pause(0.01)
+        # cnter +=1
 
-    def modes(timestamps, modes):
-        fig4.plot(timestamps, modes, 'orange')
-        fig4.set_title('Modes', font_dict)
-        plt.pause(0.01)
-
-    # plt.legend()
-    # fig1.get_legend()
     plt.show()
 
 
